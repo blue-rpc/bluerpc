11e23296a1ebbfd80f99e96088f20e2bddcce83d
stan <stitrzv@proton.me>
2024-01-25 18:26:02 +0200

    fixed problem with nested procedures

--- auth_test.go
+++ auth_test.go
@@ -115,3 +115,62 @@ func TestAuthorizedAuth(t *testing.T) {
 	fmt.Println(DefaultColors.Green + "PASSED ACCESSING A PROTECTED ROUTE WITHOUT AUTHORIZATION" + DefaultColors.Reset)
 
 }
+func TestNestedAuthorized(t *testing.T) {
+
+	fmt.Println(DefaultColors.Green + "TESTING AUTHORIZATION" + DefaultColors.Reset)
+
+	fmt.Println(DefaultColors.Green + "TESTING ACCESSING A PROTECTED ROUTE WITHOUT AUTHORIZATION " + DefaultColors.Reset)
+
+	test_token := "test_token"
+	app := New(&Config{
+		OutputPath:          "./some-file.ts",
+		DisableInfoPrinting: true,
+		DisableGenerateTS:   true,
+		Authorizer: NewAuth(func(ctx *Ctx) (any, error) {
+			bearer := ctx.Get("Authorization")
+
+			token := "Bearer " + test_token
+			if bearer != token {
+				return nil, fmt.Errorf("Unauthorized")
+			}
+			return User{
+				Name: "hello",
+			}, nil
+		}),
+	})
+
+	proc := NewQuery[any, any](app, func(ctx *Ctx, query any) (*Res[any], error) {
+		return &Res[any]{
+			Status: 200,
+			Header: Header{},
+			Body: group_test_output{
+				FieldOneOut:   "dwa",
+				FieldTwoOut:   "dwadwa",
+				FieldThreeOut: "dwadwadwa",
+			},
+		}, nil
+	})
+
+	protectedRoute := app.Router("/protected-nested").Protected()
+	unprotectedRoute := app.Router("/unprotected-nested")
+
+	proc.Attach(protectedRoute, "/test")
+	proc.Attach(unprotectedRoute, "/test")
+
+	req, err := http.NewRequest("GET", "http://localhost:3000/protected-nested/test", nil)
+
+	if err != nil {
+		t.Fatalf(DefaultColors.Red+"Could not create a new request : %s", err.Error())
+	}
+	req.Header.Set("Authorization", "Bearer "+test_token)
+
+	res, err := app.Test(req)
+	if err != nil {
+		t.Fatalf(DefaultColors.Red+"Could not do the request : %s", err.Error())
+	}
+
+	if res.StatusCode > 400 {
+		t.Fatalf(DefaultColors.Red + "Response status code for authorized access was over 400 " + DefaultColors.Reset)
+	}
+
+}
